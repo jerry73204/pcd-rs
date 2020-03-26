7b3f8fcd2a3788a20e6cc25afabf208d4b421a9d
jerry73204 <jerry73204@gmail.com>
2020-03-27 04:02:55 +0800

    Rename pcd-rs-derive modules

--- pcd-rs-derive/src/lib.rs
+++ pcd-rs-derive/src/lib.rs
@@ -6,8 +6,8 @@ extern crate quote;
 extern crate syn;
 extern crate regex;
 
-mod derive_read;
-mod derive_write;
+mod derive_de;
+mod derive_ser;
 
 use proc_macro::TokenStream;
 use syn::DeriveInput;
@@ -18,7 +18,7 @@ use syn::DeriveInput;
 #[proc_macro_derive(PcdDeserialize, attributes(pcd_rename, pcd_ignore_name))]
 pub fn pcd_record_read_derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
-    let derive_read_tokens = derive_read::f_pcd_record_read_derive(input)
+    let derive_read_tokens = derive_de::f_pcd_record_read_derive(input)
         .unwrap_or_else(|err| err.to_compile_error().into());
     TokenStream::from(derive_read_tokens)
 }
@@ -29,7 +29,7 @@ pub fn pcd_record_read_derive(input: TokenStream) -> TokenStream {
 #[proc_macro_derive(PcdSerialize, attributes(pcd_rename))]
 pub fn pcd_record_write_derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
-    let derive_write_tokens = derive_write::f_pcd_record_write_derive(input)
+    let derive_write_tokens = derive_ser::f_pcd_record_write_derive(input)
         .unwrap_or_else(|err| err.to_compile_error().into());
     TokenStream::from(derive_write_tokens)
 }
