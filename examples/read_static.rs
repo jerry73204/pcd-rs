48a5d64c48f8754f975c5e36298beaa49a9ad1e3
jerry73204 <jerry73204@gmail.com>
2020-06-29 20:32:58 +0800

    Check derive feature flag in examples

--- examples/read_static.rs
+++ examples/read_static.rs
@@ -1,17 +1,30 @@
-use anyhow::Result;
-use pcd_rs::{PcdDeserialize, Reader, ReaderBuilder};
+#[cfg(feature = "derive")]
+mod example {
+    use anyhow::Result;
+    use pcd_rs::{PcdDeserialize, Reader, ReaderBuilder};
 
-#[derive(PcdDeserialize)]
-pub struct Point {
-    pub x: f32,
-    pub y: f32,
-    pub z: f32,
-    pub rgb: f32,
+    #[derive(PcdDeserialize)]
+    pub struct Point {
+        pub x: f32,
+        pub y: f32,
+        pub z: f32,
+        pub rgb: f32,
+    }
+
+    pub fn main() -> Result<()> {
+        let reader: Reader<Point, _> = ReaderBuilder::from_path("test_files/ascii.pcd")?;
+        let points = reader.collect::<Result<Vec<_>>>()?;
+        println!("{} points found", points.len());
+        Ok(())
+    }
 }
 
-fn main() -> Result<()> {
-    let reader: Reader<Point, _> = ReaderBuilder::from_path("test_files/ascii.pcd")?;
-    let points = reader.collect::<Result<Vec<_>>>()?;
-    println!("{} points found", points.len());
-    Ok(())
+#[cfg(feature = "derive")]
+fn main() -> anyhow::Result<()> {
+    example::main()
+}
+
+#[cfg(not(feature = "derive"))]
+fn main() {
+    panic!(r#"please enable "derive" feature to run this example"#);
 }
--- examples/write_static.rs
+++ examples/write_static.rs
@@ -1,46 +1,59 @@
-use anyhow::Result;
-use pcd_rs::{DataKind, PcdDeserialize, PcdSerialize, Writer, WriterBuilder};
+#[cfg(feature = "derive")]
+mod example {
+    use anyhow::Result;
+    use pcd_rs::{DataKind, PcdDeserialize, PcdSerialize, Writer, WriterBuilder};
 
-#[derive(Debug, PcdDeserialize, PcdSerialize, PartialEq)]
-pub struct Point {
-    #[pcd_rename("new_x")]
-    x: f32,
-    y: [u8; 3],
-    z: i32,
-}
-
-fn main() -> Result<()> {
-    // output path
-    let path = "test_files/dump_ascii_static.pcd";
-
-    // point data
-    let dump_points = vec![
-        Point {
-            x: 3.14159,
-            y: [2, 1, 7],
-            z: -5,
-        },
-        Point {
-            x: -0.0,
-            y: [254, 6, 98],
-            z: 7,
-        },
-        Point {
-            x: 5.6,
-            y: [4, 0, 111],
-            z: -100000,
-        },
-    ];
-
-    // serialize points
-    let mut writer: Writer<Point, _> =
-        WriterBuilder::new(300, 1, Default::default(), DataKind::ASCII)?.create(path)?;
-
-    for point in dump_points.iter() {
-        writer.push(&point)?;
+    #[derive(Debug, PcdDeserialize, PcdSerialize, PartialEq)]
+    pub struct Point {
+        #[pcd_rename("new_x")]
+        x: f32,
+        y: [u8; 3],
+        z: i32,
     }
 
-    writer.finish()?;
+    pub fn main() -> Result<()> {
+        // output path
+        let path = "test_files/dump_ascii_static.pcd";
 
-    Ok(())
+        // point data
+        let dump_points = vec![
+            Point {
+                x: 3.14159,
+                y: [2, 1, 7],
+                z: -5,
+            },
+            Point {
+                x: -0.0,
+                y: [254, 6, 98],
+                z: 7,
+            },
+            Point {
+                x: 5.6,
+                y: [4, 0, 111],
+                z: -100000,
+            },
+        ];
+
+        // serialize points
+        let mut writer: Writer<Point, _> =
+            WriterBuilder::new(300, 1, Default::default(), DataKind::ASCII)?.create(path)?;
+
+        for point in dump_points.iter() {
+            writer.push(&point)?;
+        }
+
+        writer.finish()?;
+
+        Ok(())
+    }
+}
+
+#[cfg(feature = "derive")]
+fn main() -> anyhow::Result<()> {
+    example::main()
+}
+
+#[cfg(not(feature = "derive"))]
+fn main() {
+    panic!(r#"please enable "derive" feature to run this example"#);
 }
